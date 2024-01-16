20512a7f91610165000ceed0e783a541f974508d
unknown <jleya95@gmail.com>
2024-01-15 23:17:42 -0500

    agent_j site, started building database

---
+++ agentjSite/dotnet/database/agentjSite.sql
@@ -0,0 +1,31 @@
+USE master
+GO
+
+--drop database if it exists
+IF DB_ID('agent_j') IS NOT NULL
+BEGIN
+	ALTER DATABASE agent_j SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE agent_j;
+END
+
+CREATE DATABASE agent_j
+GO
+
+USE agent_j
+GO
+
+--create tables
+CREATE TABLE users (
+	user_id int IDENTITY(1,1) NOT NULL,
+	username varchar(50) NOT NULL,
+	password_hash varchar(200) NOT NULL,
+	salt varchar(200) NOT NULL,
+	user_role varchar(50) NOT NULL
+	CONSTRAINT PK_user PRIMARY KEY (user_id)
+)
+
+--populate default data
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+GO
