62e0b26f5461532ba7e3a90253a58edef39e46b4
unknown <jleya95@gmail.com>
2024-02-20 01:04:42 -0500

    Front end & back end are talking

--- RecordCollection/vue/src/components/HomeComponent.vue
+++ RecordCollection/vue/src/components/HomeComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-      <h1>Select an Option:</h1>
+      <h1>Record Selector</h1>
       <button @click="randomPagePush()">Random</button>
       <button>All</button>
     </div>
--- RecordCollection/vue/src/components/RandomRecordComponent.vue
+++ RecordCollection/vue/src/components/RandomRecordComponent.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>{{ record.artist }}</p>
+    <div class="random-record">
+        <p>{{ record.artist }}  "{{ record.title }}"  {{ record.releaseYear }} {{ record.label }} {{ record.issueYear }}</p>
+        <button @click="refreshPage()">Go Again</button>    
+    </div>
 </template>
 
 <script>
@@ -8,29 +11,33 @@ import RecordsService from '@/services/RecordsService.js'
 export default {
     data() {
         return {
-            record: {
-                id: 0,
-                file: "",
-                artist: "",
-                title: "",
-                releaseYear: 0,
-                label: "",
-                issueYear: 0,
-                serialNumber: "",
-                pressing: "",
-                discNumber: 0,
-                color: "",
-                notes: "",
-                needleInfo: ""
-            }
+            // record: {
+            //     id: 0,
+            //     file: "",
+            //     artist: "",
+            //     title: "",
+            //     releaseYear: 0,
+            //     label: "",
+            //     issueYear: 0,
+            //     serialNumber: "",
+            //     pressing: "",
+            //     discNumber: 0,
+            //     color: "",
+            //     notes: "",
+            //     needleInfo: ""
+            // }
+            record: []
         }
     },
     methods: {
         getRandomRecord() {
-            let object = RecordsService.getRandomRecord();
-            this.record.artist = object.artist;
-
-            return this.record;
+            RecordsService.getRandomRecord()
+            .then(response => {
+                this.record = response.data
+            })
+        },
+        refreshPage() {
+            location.reload();
         }
     },
     created() {
