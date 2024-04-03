26a2511963279df37d8a53f27b323758f027d3e1
unknown <jleya95@gmail.com>
2024-04-03 00:36:17 -0400

    AJS work

--- agentjSite/dotnet/Capstone/Services/SpotifyService.cs
+++ agentjSite/dotnet/Capstone/Services/SpotifyService.cs
@@ -7,11 +7,23 @@ namespace agentjSite.Services
 {
     public class SpotifyService : ISpotifyService
     {
+        const string clientID = "4429774265a942b8b9bbc1157be39562";
+        const string clientSecret = "7e791cb9a1d54071ba64e4aad9db0efa";
         private static RestClient client = new RestClient();
         private RestClientOptions options = new RestClientOptions(baseUrl: "https://api.spotify.com")
         {
-/*            Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator()
-*/        };
-        
+            Authenticator = new HttpBasicAuthenticator(clientID, clientSecret)
+        };
+
+        public SpotifyService() { 
+        if (client == null)
+            {
+                client = new RestClient(options);
+            }
+        }
+
+
+
+        public RestRequest request = new RestRequest().AddHeader("", "").AddParameter("", "");
     }
 }
